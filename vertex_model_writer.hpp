51c284330a4bec133615b113479a7120c7bec281
Juho Lauri <euler314@users.noreply.github.com>
2017-07-24 15:27:53 +0100

    Update vertex_model_writer.hpp

--- vertex_model_writer.hpp
+++ vertex_model_writer.hpp
@@ -6,7 +6,19 @@
 
 class vertex_model_writer : public model_writer
 {
-	
+public:
+	vertex_model_writer(const graph& g, index_t k, std::ostream& os, const std::string& comment = "%")
+		: model_writer(g, k, os, comment)
+	{
+
+	}
+
+private:
+	virtual void impl_preprocess();
+	virtual void impl_process();
+	virtual void impl_postprocess();
+
+	virtual void impl_process_vertex_pair(index_t u, index_t v);
 };
 
 #endif
